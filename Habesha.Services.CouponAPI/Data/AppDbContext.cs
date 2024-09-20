d090bf1637931a9849f593c81c7c52127ed35567
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-21 01:21:49 +0300

    Create AppDbContext

---
+++ Habesha.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Habesha.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Habesha.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Habesha.Services.CouponAPI/Models/Coupon.cs
+++ Habesha.Services.CouponAPI/Models/Coupon.cs
@@ -1,9 +1,14 @@
-﻿namespace Habesha.Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Habesha.Services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
+        [Required]
         public string CouponCode { get; set; }
+        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
