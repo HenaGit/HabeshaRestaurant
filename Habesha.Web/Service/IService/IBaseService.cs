002c88d359fde1cd3b27c75423fb4db1b36f612d
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-22 00:30:16 +0300

    Base Service Implementation

---
+++ Habesha.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Habesha.Web.Models;
+
+namespace Habesha.Web.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
