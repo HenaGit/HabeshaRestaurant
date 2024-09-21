d0d84194d9df57fadc01a8e5113891dccb0c762e
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-21 22:45:28 +0300

    Get all and Get Coupon By Id

---
+++ Habesha.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,45 @@
+﻿using Habesha.Services.CouponAPI.Data;
+using Habesha.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Habesha.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u => u.CouponId == id);
+                return objList;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+    }
+}
