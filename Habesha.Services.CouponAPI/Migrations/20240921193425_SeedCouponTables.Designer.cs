7fdc856f682326cf4343d49a47212da37dcdef50
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-21 22:37:09 +0300

    Seed Database

--- Habesha.Services.CouponAPI/Migrations/20240921193425_SeedCouponTables.Designer.cs
+++ Habesha.Services.CouponAPI/Migrations/20240921193425_SeedCouponTables.Designer.cs
@@ -3,6 +3,7 @@ using Habesha.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Habesha.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240921193425_SeedCouponTables")]
+    partial class SeedCouponTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
