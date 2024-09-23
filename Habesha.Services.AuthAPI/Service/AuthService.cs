70f5d6fdb1c8c8dbd13156583e9ab2f8087e8e30
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-23 23:35:11 +0300

    Add Register Endpoint in Auth Service

---
+++ Habesha.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,58 @@
+﻿using Habesha.Services.AuthAPI.Data;
+using Habesha.Services.AuthAPI.Models;
+using Habesha.Services.AuthAPI.Models.Dto;
+using Habesha.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Habesha.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AuthService(AppDbContext db,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registrationRequestDto.Email,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                Name = registrationRequestDto.Name,
+                PhoneNumber = registrationRequestDto.PhoneNumber
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registrationRequestDto.Email);
+                    UserDto userDto = new()
+                    {
+                        Email = userToReturn.Email,
+                        ID = userToReturn.Id,
+                        Name = userToReturn.Name,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+                    return userDto;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return new UserDto();
+        }
+    }
+}
---
+++ Habesha.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Habesha.Services.AuthAPI.Models.Dto;
+
+namespace Habesha.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
