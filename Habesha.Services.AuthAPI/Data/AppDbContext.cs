8c70adea08f4486f4e72f57aec7cbbb396a08842
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-23 22:44:32 +0300

    Add DbContext and Create Identity Tables

---
+++ Habesha.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Habesha.Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
