ca54e43156fb55d2947bf340a36f61e1da30dca5
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-23 22:54:45 +0300

    Add custom properties to users table

--- Habesha.Services.AuthAPI/Data/AppDbContext.cs
+++ Habesha.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Habesha.Services.AuthAPI.Models;
 
 namespace Habesha.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
