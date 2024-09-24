d33101dfe741fc1b06a334ee09c90b74ab92d022
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-24 22:42:22 +0300

    Register in Action

--- Habesha.Services.AuthAPI/Program.cs
+++ Habesha.Services.AuthAPI/Program.cs
@@ -1,5 +1,7 @@
 using Habesha.Services.AuthAPI.Data;
 using Habesha.Services.AuthAPI.Models;
+using Habesha.Services.AuthAPI.Service.IService;
+using Habesha.Services.AuthAPI.Service;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +16,7 @@ builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSett
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IAuthService, AuthService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
