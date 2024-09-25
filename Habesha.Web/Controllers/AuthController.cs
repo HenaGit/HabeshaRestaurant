7b2d5f3d7d22ccabecdbe7bb7d9fda5482606e29
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-26 01:54:03 +0300

    Add Logout in Action

--- Habesha.Web/Controllers/AuthController.cs
+++ Habesha.Web/Controllers/AuthController.cs
@@ -81,9 +81,11 @@ namespace Habesha.Web.Controllers
             ViewBag.RoleList = roleList;
             return View(obj);
         }
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            _tokenProvider.ClearToken();
+            return RedirectToAction("Index", "Home");
         }
         private async Task SignInUser(LoginResponseDto model)
         {
