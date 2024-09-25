b5723a700e84aea18b26f1cf5da89f88c94e7d8c
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-25 21:14:43 +0300

    Add Auth Controller in Web Project

---
+++ Habesha.Web/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Habesha.Web.Models;
+using Habesha.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Habesha.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
