c408cee57bd4a8949ee128e6805cdf9b18b1cdc7
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-25 21:06:30 +0300

    Add Auth Service in Web Project

---
+++ Habesha.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Habesha.Web.Models;
+
+namespace Habesha.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto);
+    }
+}
