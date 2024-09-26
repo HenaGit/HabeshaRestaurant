727f781924d346396c92f2e297397933f97a215d
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-09-26 20:59:01 +0300

    Product API in Action

---
+++ Habesha.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Habesha.Services.ProductAPI.Models;
+using Habesha.Services.ProductAPI.Models.Dto;
+
+namespace Habesha.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
